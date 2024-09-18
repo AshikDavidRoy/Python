6d26c897ee14d602d6de33bc3f728cda6c4a2692
Ashik <ashikdavidroy0696@gmail.com>
2024-09-18 21:34:28 +0530

    (fect): studied Exceptions Handling

--- Learn_the_Basics/Exceptions.py
+++ Learn_the_Basics/Exceptions.py
@@ -13,3 +13,14 @@ except ZeroDivisionError:
     print("Cannot divide by zero!")
 finally:    
     print("This will always run.")
+
+
+def check_age(age):
+    if age < 0:
+        raise ValueError("Age cannot be negative!")
+    return age
+
+try:
+    print(check_age(-5))
+except ValueError as e:
+    print(e)
