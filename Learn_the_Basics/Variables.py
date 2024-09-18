72712899696f8ff048f623cfc29a4390b30e47fe
Ashik <ashikdavidroy0696@gmail.com>
2024-09-18 19:27:12 +0530

    (fect): studied Basic Syntax,Variables and Data Types, Lists, Tuples, Sets, Dictionaries and Conditionals

---
+++ Learn_the_Basics/Variables.py
@@ -0,0 +1,16 @@
+name = "Alice"   # String variable
+age = 30         # Integer variable
+height = 5.6     # Float variable
+is_student = True  # Boolean variable
+
+print(name,type(name))  # <class 'str'>
+print(age,type(age))   # <class 'int'>
+print(height,type(height)) # <class 'float'>
+print(is_student,type(is_student)) # <class 'bool'>
+
+# Arithmetic Operations
+sum = age + 5       # Addition
+product = height * 2  # Multiplication
+
+print(sum)
+print(product)
---
+++ Learn_the_Basics/collections.py
@@ -0,0 +1,14 @@
+my_list = [1, 2, 3]
+my_list.append(4)    # Adds 4 to the end of the list
+print(my_list)       # Output: [1, 2, 3, 4]
+
+my_tuple = (1, 2, 3)
+print(my_tuple)     # Output: (1, 2, 3)
+
+my_set = {1, 2, 3}
+my_set.add(4)       # Adds 4 to the set
+print(my_set)       # Output: {1, 2, 3, 4}
+
+my_dict = {"name": "Alice", "age": 30}
+my_dict["height"] = 5.6    # Adds a new key-value pair
+print(my_dict)            # Output: {"name": "Alice", "age": 30, "height": 5.6}
